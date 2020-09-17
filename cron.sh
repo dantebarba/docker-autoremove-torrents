9f9f4f6052884575a9888bfb0eb54ff40b251aef
Dante Barba <dantebarba@Dantes-iMac.local>
2020-09-17 14:25:58 -0300

    new config for autoremove-torrents. Allows crond

--- cron.sh
+++ cron.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-printenv | cat - /etc/cron.d/cron-jobs > ~/crontab.tmp \
-    && mv ~/crontab.tmp /etc/cron.d/cron-jobs
+if [ -z "$CRON" ]
+  then
+    echo "INFO: No CRON setting found. Running autoremove once."
+    /usr/bin/autoremove-torrents $OPTS
+  else
+    # Setup cron schedule
+    crontab -d
+    echo "$CRON /usr/bin/autoremove-torrents $OPTS >> /var/log/autoremove-torrents.log 2>&1" > /tmp/crontab.tmp
+    crontab /tmp/crontab.tmp
+    rm /tmp/crontab.tmp
 
-chmod 644 /etc/cron.d/cron-jobs
-
-tail -f /var/log/cron.log &
-
-cron -f
+    # Start cron
+    echo "INFO: Starting crond ..."
+    touch /var/log/crond.log
+    crond -b -l 0 -L /var/log/crond.log
+    echo "INFO: crond started"
+    tail -F /var/log/crond.log /var/log/autoremove-torrents.log
+  fi
+fi
